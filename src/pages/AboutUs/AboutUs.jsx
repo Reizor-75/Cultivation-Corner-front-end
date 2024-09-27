32d37e7063f5abe8cf6f3ce1dc8db41f27150106
mlinares <m.linares93@gmail.com>
2024-09-27 12:09:00 -0400

    block out about us page

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -5,6 +5,21 @@ const AboutUs = () => {
   return (
     <main>
       <h1>About Us</h1>
+      <div className={styles.shop_Image}> shop image Place holder</div>
+      <div className={styles.blurb}> shop story</div>
+
+      <div className={styles.staff_Container}>
+        <h1>meet the staff</h1>
+        <div className={styles.staffProfile}>
+          <div className={styles.staffMemPhoto}>Staff Member picture placeHolder</div>
+          <div className={styles.staffMemInfo}>
+            <div className={styles.staffMemName}>
+              NAME
+            </div>
+            <div className={styles.staffMemBio}>About Me</div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
