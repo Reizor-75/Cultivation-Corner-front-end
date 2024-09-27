6fd238ffdb25fd72ef48a3eb7edaf9e27c3a1c42
mlinares <m.linares93@gmail.com>
2024-09-26 23:25:11 -0400

    style nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,27 @@
 // npm modules
 import { NavLink } from 'react-router-dom'
 
+// css
+import styles from './NavBar.module.css'
+
 const NavBar = ({ user, handleLogout }) => {
   return (
     <nav>
-      {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
-        </ul>
-      :
-        <ul>
-          <li><NavLink to="/auth/login">Log In</NavLink></li>
-          <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-        </ul>
-      }
+      <div className={styles.nav_container}>
+        {user ?
+          <ul>
+            <li>Welcome, {user.name}</li>
+            <li><NavLink to="/profiles">Profiles</NavLink></li>
+            <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
+            <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
+          </ul>
+        :
+          <ul>
+            <li><NavLink to="/auth/login">Log In</NavLink></li>
+            <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
+          </ul>
+        }
+      </div>
     </nav>
   )
 }
