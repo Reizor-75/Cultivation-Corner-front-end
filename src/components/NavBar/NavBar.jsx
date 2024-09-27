fbdef12568a6d8e8c53f3f4d3581ad6f92a1aa61
mlinares <m.linares93@gmail.com>
2024-09-27 11:16:19 -0400

    update views for navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,23 +9,29 @@ const NavBar = ({ user, handleLogout }) => {
     <nav>
       <div className={styles.nav_container}>
         <div className={styles.site_Title}>Cultivation Corner</div>
-        {user ?
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><NavLink to="/profiles">Profiles</NavLink></li>
-            <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
+           <ul>
+            {/* <li>Welcome, {user.name}</li> */}
+            {/* <li><NavLink to="/profiles">Profiles</NavLink></li>
             <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
+             */}
           </ul>
-        :
           <ul>
             <li><NavLink to="/AboutUs">About Us</NavLink></li>
             <li>Shop</li>
             <li>Services</li>
             <li>Blog</li>
-            <li><NavLink to="/auth/login">Log In</NavLink></li>
-            <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
+            
+            {user ?
+              <>
+                <li><NavLink to="" onClick={handleLogout}><i className="fa-solid fa-circle-user"></i></NavLink></li>
+              </>
+              :
+              <>
+                <li><NavLink to="/auth/signup">Sign Up</NavLink></li>                
+              <li><NavLink to="/auth/login">Log In</NavLink></li>
+              </>
+            }
           </ul>
-        }
       </div>
     </nav>
   )
