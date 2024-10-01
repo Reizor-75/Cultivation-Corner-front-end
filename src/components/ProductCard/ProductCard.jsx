a192ed7e2b2c8a6c713d5e60d93e6b5e8cc71a60
mlinares <m.linares93@gmail.com>
2024-10-01 14:17:20 -0400

    stub up productCard component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,10 +1,15 @@
 // css
 import styles from './ProductCard.module.css'
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
   return (
-    <>  
-    </>
+    <div className={styles.product_container}>
+      <img src="" alt="Product image" />
+      <div className={styles.product_info}>
+        <h1>{product.name ?product.name : "Not Availible" }</h1>
+        <div className={styles.price}>{product.price ? product.price : "contact shop for price"}</div>
+      </div>
+    </div>
   )
 }
 
