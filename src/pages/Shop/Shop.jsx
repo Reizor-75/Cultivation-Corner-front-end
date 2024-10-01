096bf5768256b4bc2e0356e7a13cfef2bbdd6e66
mlinares <m.linares93@gmail.com>
2024-10-01 12:54:46 -0400

    import npm modules, set up state for shop page

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,11 +1,20 @@
+// npm module
+import { useEffect, useState } from 'react'
+
 // css
 import styles from './Shop.module.css'
 
-const Shop = ({ user }) => {
+const Shop = () => {
+  const [products, setProducts] = useState()
+
+  useEffect(() => {
+  
+  }, [])
+
   return (
     <main className={styles.container}>
       <div className={styles.shop_container}>
-        
+        <h1>Products</h1>
       </div>
     </main>
   )
