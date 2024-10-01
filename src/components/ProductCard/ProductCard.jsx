87558a8a2a47ac5c951377ddfa2e0092807109c2
mlinares <m.linares93@gmail.com>
2024-10-01 14:38:43 -0400

    style product card

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,10 +4,10 @@ import styles from './ProductCard.module.css'
 const ProductCard = ({product}) => {
   return (
     <div className={styles.product_container}>
-      <img src="" alt="Product image" />
+      <img className= {styles.product_image} src="" alt="Product image" />
       <div className={styles.product_info}>
         <h1>{product.productName ?product.productName : "Not Availible" }</h1>
-        <div className={styles.price}>{product.price ? product.price : "contact shop for price"}</div>
+        <div className={styles.price}>{product.price ? "$ " + product.price : "contact shop for price"}</div>
       </div>
     </div>
   )
