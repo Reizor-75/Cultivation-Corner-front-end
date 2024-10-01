99f5dbdb6a57e83dec2596de2107bf6efff00d71
mlinares <m.linares93@gmail.com>
2024-09-30 23:05:40 -0400

    set up template for landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,6 +5,22 @@ const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
       <h1>hello, {user ? user.name : 'friend'}</h1>
+
+      <div className={styles.landing_container}>
+      <img src="" alt="slideshow of Plant Shop" />
+      <div className={styles.shop_details}>
+        Navigation to Shop
+        <div className="map">Map</div>
+        <div className={styles.shop_info}>
+          <div className="shop_hours">
+            <h2>Hours</h2>
+          </div>
+          <div className="shop_address">
+            <h2>Address</h2>
+          </div>
+        </div>
+      </div>
+      </div>
     </main>
   )
 }
