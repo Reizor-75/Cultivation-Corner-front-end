1c679fc9b2339522b486d2764520f4f2272fbd04
mlinares <m.linares93@gmail.com>
2024-10-01 12:44:47 -0400

    create shop page

---
+++ src/pages/Shop/Shop.jsx
@@ -0,0 +1,14 @@
+// css
+import styles from './Shop.module.css'
+
+const Shop = ({ user }) => {
+  return (
+    <main className={styles.container}>
+      <div className={styles.shop_container}>
+        
+      </div>
+    </main>
+  )
+}
+
+export default Shop
