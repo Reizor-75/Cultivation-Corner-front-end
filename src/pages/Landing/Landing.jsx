ae252192e95bfa41ddd910df41ae30f3b99c55a7
mlinares <m.linares93@gmail.com>
2024-09-30 23:14:15 -0400

    style landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -7,19 +7,22 @@ const Landing = ({ user }) => {
       <h1>hello, {user ? user.name : 'friend'}</h1>
 
       <div className={styles.landing_container}>
-      <img src="" alt="slideshow of Plant Shop" />
-      <div className={styles.shop_details}>
-        Navigation to Shop
-        <div className="map">Map</div>
-        <div className={styles.shop_info}>
-          <div className="shop_hours">
-            <h2>Hours</h2>
-          </div>
-          <div className="shop_address">
-            <h2>Address</h2>
+        <img src="" alt="slideshow of Plant Shop" />
+        <div className={styles.shop_details}>
+          <div className={styles.map}>Map</div>
+          <div className={styles.shop_info}>
+            <div className="shop_hours">
+              <h2>Hours</h2>
+              <ul className={styles.hours}>
+                <li>Sun: 10:00 -6:00</li>
+                <li>Mon-Fri: 10:00-7:00</li>
+              </ul>
+            </div>
+            <div className="shop_address">
+              <h2>Address</h2>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </main>
   )
