5fd19132748b1997fb5ab492618dc96842f92c27
mlinares <m.linares93@gmail.com>
2024-09-30 22:58:11 -0400

    add description of services provided

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -6,8 +6,12 @@ const Services = () => {
     <main>
       <div className={styles.services_container}>
         <h1>Services</h1>
-        <div className={styles.service}>
+        <p>
+          Cultivation Corner offers a diverse range of services designed to enhance plant care and gardening experiences. These services typically include the sale of various indoor and outdoor plants, gardening supplies, and decorative pots. Additionally, the shop may provide expert advice on plant selection, care tips, and pest management, helping customers choose the right plants for their environment. Workshops and classes on topics like plant propagation, terrarium building, and seasonal gardening can also be available, fostering a community of plant enthusiasts. Furthermore, delivery and potting services may be offered to ensure a seamless experience for customers looking to beautify their spaces. <br/>
+        </p>
 
+        <p>Check out our upcoming classes</p>
+        <div className={styles.service}>
           Repotting service
           Plant Care Basics class
           Terrarium Making class
