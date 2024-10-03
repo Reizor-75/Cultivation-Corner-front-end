21eaee6419a921a609a0b5638065278b007459bc
mlinares <m.linares93@gmail.com>
2024-10-02 22:32:57 -0400

    create addProduct view

---
+++ src/pages/AddProduct/AddProduct.jsx
@@ -0,0 +1,15 @@
+
+// css
+import styles from './AddProduct.module.css' 
+
+const AddProduct = () => {
+  return ( 
+    <main>
+      <div className={styles.add_product_container}>
+        <h1>Add Product</h1>
+      </div>
+    </main>
+  );
+}
+
+export default AddProduct;
