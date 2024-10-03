575838222910779abea200cf3c995e2816795875
mlinares <m.linares93@gmail.com>
2024-10-03 13:30:27 -0400

    create add product form

--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -1,13 +1,123 @@
+// npm module
+import { useState, useRef } from 'react'
+import { Link } from 'react-router-dom'
 
 // css
 import styles from './AddProduct.module.css' 
 
 const AddProduct = () => {
-  return ( 
-    <main>
-      <div className={styles.add_product_container}>
-        <h1>Add Product</h1>
-      </div>
+  const imgInputRef = useRef(null)
+
+  const [message, setMessage] = useState('')
+  const [formData, setFormData] = useState({
+    productName: '',
+    quantity: '',
+    price: '',
+    productType: '',
+  })
+  const [photoData, setPhotoData] = useState({ photo: null })
+
+  const handleChangePhoto = evt => {
+    const file = evt.target.files[0]
+    let isFileInvalid = false
+    let errMsg = ""
+    const validFormats = ['gif', 'jpeg', 'jpg', 'png', 'svg', 'webp']
+    const photoFormat = file.name.split('.').at(-1)
+
+    // cloudinary supports files up to 10.4MB each as of May 2023
+    if (file.size >= 10485760) {
+      errMsg = "Image must be smaller than 10.4MB"
+      isFileInvalid = true
+    }
+    if (!validFormats.includes(photoFormat)) {
+      errMsg = "Image must be in gif, jpeg/jpg, png, svg, or webp format"
+      isFileInvalid = true
+    }
+    
+    setMessage(errMsg)
+    
+    if (isFileInvalid) {
+      imgInputRef.current.value = null
+      return
+    }
+
+    setPhotoData({ photo: evt.target.files[0] })
+  }
+
+  const handleChange = evt => {
+    setMessage('')
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSubmit = async evt => {
+    evt.preventDefault()
+  }
+
+  const { productName, quantity, price, productType } = formData
+
+  return (
+    <main className={styles.container}>
+      <h1>Add New Product</h1>
+      <p className={styles.message}>{message}</p>
+      <form autoComplete="off" onSubmit={handleSubmit} className={styles.form}>
+        <label className={styles.label}>
+          Product Name
+          <input
+            type="text"
+            value={productName}
+            name="productName"
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label className={styles.label}>
+          Quantity
+          <input
+            type="number"
+            value={quantity}
+            name="quantity"
+            onChange={handleChange}
+          />
+        </label>
+        <label className={styles.label}>
+          Price
+          <input
+            type="number"
+            value={price}
+            name="price"
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label className={styles.label}>
+          Product Type
+          <select 
+            name={productType} 
+          >
+            <option value="Plant">Plant</option>
+            <option value="Soil">Soil</option>
+            <option value="fertilizer">Fertilizer</option>
+            <option value="planters">Planter</option>
+          </select>
+        </label>
+        <label className={styles.label}>
+          Upload Photo
+          <input 
+            type="file" 
+            name="photo" 
+            onChange={handleChangePhoto}
+            ref={imgInputRef}
+          />
+        </label>
+        <div>
+          <Link to="/">Cancel</Link>
+          <button
+            className={styles.button}
+          >
+            Add Product
+          </button>
+        </div>
+      </form>
     </main>
   );
 }
