0287a6ef90021d6b20f27c1aeb32360c7d8e5af7
mlinares <m.linares93@gmail.com>
2024-10-03 23:43:56 -0400

    fetch product data to view

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,10 +1,29 @@
+// npm modules
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+// services
+import * as productServices from '../../services/productService'
+
 // css
 import styles from './ProductDetails.module.css'
 
-const ProductDetails = () => {
+const ProductDetails = ({user}) => {
+  const {productId} = useParams()
+  const [product, setProduct] = useState()
+
+  useEffect(() =>{
+    const fetchWorkshop= async () => {
+      const data = await productServices.showproduct(productId)
+      setProduct(data)
+    }
+    fetchWorkshop()
+  }, [productId])
+
+
   return (  
     <main>
-      <h1>Product Details</h1>
+      <h1 className={styles.product_container}>{product.productName}</h1>
     </main>
   );
 }
