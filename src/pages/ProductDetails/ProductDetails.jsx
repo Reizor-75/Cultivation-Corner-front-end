01bbee8d24332dbcfef700ceb6e1dbef4c061231
mlinares <m.linares93@gmail.com>
2024-10-03 23:32:20 -0400

    create product details view

---
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,12 @@
+// css
+import styles from './ProductDetails.module.css'
+
+const ProductDetails = () => {
+  return (  
+    <main>
+      <h1>Product Details</h1>
+    </main>
+  );
+}
+
+export default ProductDetails
