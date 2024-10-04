a2801e7eff13f6e94539c7c1152118d6a5c7756e
mlinares <m.linares93@gmail.com>
2024-10-03 23:36:54 -0400

    create navlink in productCard component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,17 +1,22 @@
+// npm modules
+import { NavLink } from 'react-router-dom'
+
 // css
 import styles from './ProductCard.module.css'
 
 const ProductCard = ({user, product}) => {
   return (
-    <div className={styles.product_container}>
-      <img className= {styles.product_image} src="" alt="Product image" />
-      <div className={styles.product_info}>
-        <h1>{product.productName ?product.productName : "Not Availible" }</h1>
-        <div className={styles.bottom_row}>{product.price ? "$ " + product.price : "contact shop for price"}
-          {user.role >= 500 && <button>Edit</button> }
+    <NavLink to={`/Shop/${product._id}`}>
+      <div className={styles.product_container}>
+        <img className= {styles.product_image} src="" alt="Product image" />
+        <div className={styles.product_info}>
+          <h1>{product.productName ?product.productName : "Not Availible" }</h1>
+          <div className={styles.bottom_row}>{product.price ? "$ " + product.price : "contact shop for price"}
+            {user.role >= 500 && <button>Edit</button> }
+          </div>
         </div>
       </div>
-    </div>
+    </NavLink>
   )
 }
 
