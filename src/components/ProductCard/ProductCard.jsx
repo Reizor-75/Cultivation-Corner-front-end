889aed14a3aa6c18c87f0a743d04214743f22aee
mlinares <m.linares93@gmail.com>
2024-10-03 23:06:22 -0400

    style productCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = ({user, product}) => {
       <img className= {styles.product_image} src="" alt="Product image" />
       <div className={styles.product_info}>
         <h1>{product.productName ?product.productName : "Not Availible" }</h1>
-        <div className={styles.price}>{product.price ? "$ " + product.price : "contact shop for price"}
+        <div className={styles.bottom_row}>{product.price ? "$ " + product.price : "contact shop for price"}
           {user.role >= 500 && <button>Edit</button> }
         </div>
       </div>
