aa062e07acf8303b96a4b99afd4a7388102addb5
mlinares <m.linares93@gmail.com>
2024-10-07 22:51:07 -0400

    create Blog view

---
+++ src/pages/Blogs/Blogs.jsx
@@ -0,0 +1,15 @@
+
+// css
+import styles from './Blogs.module.css'
+
+const Blogs = () => {
+  return (  
+    <main className={styles.main_container}>
+  
+    </main>
+  );
+}
+
+
+
+export default Blogs;
