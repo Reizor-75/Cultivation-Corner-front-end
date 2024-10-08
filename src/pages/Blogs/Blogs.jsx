2a975b256500dbfa304af535d34d0f3481c869ad
mlinares <m.linares93@gmail.com>
2024-10-07 22:59:59 -0400

    create Blogs template

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,11 +1,28 @@
-
+// npm modules
+import { useState, useEffect } from 'react';
 // css
 import styles from './Blogs.module.css'
 
-const Blogs = () => {
+const Blogs = ({user}) => {
+  const [blogs, setBlog] = useState([])
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      
+      setBlog()
+    }
+    fetchBlogs()
+  }, [])
+
   return (  
     <main className={styles.main_container}>
-  
+      <div className={styles.blog_container}>
+        <h1>Blog</h1>
+        <div className={styles.recent_blog}></div>
+        {blogs?.map(blog =>(
+          blog.title
+        ) )}
+      </div>
     </main>
   );
 }
