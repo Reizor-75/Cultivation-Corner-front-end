75a7a5ddb9522a2c83fbdf7a6f6b710cb5ed6d0f
mlinares <m.linares93@gmail.com>
2024-10-08 23:21:43 -0400

    load all bloags on blogs view

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,5 +1,9 @@
 // npm modules
 import { useState, useEffect } from 'react';
+
+// service
+import * as blogService from '../../services/blogService'
+
 // css
 import styles from './Blogs.module.css'
 
@@ -8,18 +12,22 @@ const Blogs = ({ user }) => {
 
   useEffect(() => {
     const fetchBlogs = async () => {
-
-      setBlog()
+      const blogsData = await blogService.getAllProducts()
+      setBlog(blogsData)
     }
     fetchBlogs()
   }, [])
+
+  if(!blogs){
+    return <h1>Loading...</h1>
+  }
 
   return (  
     <main className={styles.main_container}>
       <div className={styles.blog_container}>
         <h1>Blog</h1>
         <div className={styles.recent_blog}></div>
-        {blogs?.map(blog =>(
+        {blogs.map(blog =>(
           blog.title
         ) )}
       </div>
