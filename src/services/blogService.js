1ae9f2f45092541a3f39d28e0ef59fc46a2e8569
mlinares <m.linares93@gmail.com>
2024-10-08 23:19:58 -0400

    create getAlBlogs service Function

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -3,7 +3,17 @@ import * as tokenService from './tokenService'
 
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/blogs`
 
+async function getAllBlogs() {
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
+    })
+    return await res.json()
+  } catch (err) {
+    throw new Error(err)
+  }
+}
 
 export{
-
+  getAllBlogs,
 }
