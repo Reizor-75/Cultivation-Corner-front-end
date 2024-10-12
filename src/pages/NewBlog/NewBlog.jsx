b2c31517c7ab13153a744af3672fdac1e3094c39
mlinares <m.linares93@gmail.com>
2024-10-11 22:58:47 -0400

    create new blog view

---
+++ src/pages/NewBlog/NewBlog.jsx
@@ -0,0 +1,11 @@
+import styles from './NewBlog.module.css'
+
+const NewBlog = () => {
+  return ( 
+    <main className={styles.main_container}>
+      <h1>New Blog</h1>
+    </main>
+  );
+}
+
+export default NewBlog;
