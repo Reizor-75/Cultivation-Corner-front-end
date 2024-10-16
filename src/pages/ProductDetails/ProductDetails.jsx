b9f2255f35f1d72733a568f2e6bfca8109d1dad4
mlinares <m.linares93@gmail.com>
2024-10-15 23:27:35 -0400

    set up default values for product details view

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -25,21 +25,45 @@ const ProductDetails = ({user}) => {
     return('Loading...')
   }
 
+  let productImage = ''
+
+  if (product.photo) productImage = product.photo
+  else if(product.productType === 'plant') productImage = 'https://cdn.pixabay.com/photo/2023/11/24/18/52/cat-8410502_1280.jpg'
+  else if(product.productType === 'soil') productImage = 'https://cdn.pixabay.com/photo/2024/03/30/00/55/gardening-8663870_1280.png'
+  else if(product.productType === 'fertilizer') productImage = 'https://cdn.pixabay.com/photo/2022/11/08/14/42/monstera-7578722_1280.png'
+  else if(product.productType === 'planter') productImage = 'https://cdn.pixabay.com/photo/2024/03/24/18/32/cat-8653647_1280.png'
+  else if(product.productType === 'tool') productImage = 'https://cdn.pixabay.com/photo/2020/01/04/06/29/gardening-4739846_1280.png'
+
+
   return (  
     <main>
       
       <div className={styles.product_container}>
-        <img className={styles.product_photo} src="" alt="Plant stock photo" />
+        <img className={styles.product_photo} src={productImage} alt="Plant stock photo" />
         <div className={styles.product_info}>
-          <h1 className={styles.product_Name}>{product?.productName}
+          <h1 className={styles.product_Name}>{product.name}
           </h1>
+          <div>
+            $ {product.price}
+            {user ?
+              <button>Add To Cart</button>
+              :
+              <p>Sign up to purchase item</p>
+            }
+          </div>
           <p className={styles.product_description}>
-            {product.productDescription}
-            {product.careInstruction}
+            {product.productDescription ? product.productDescription 
+            :
+            <p>no product description available</p>
+            }
+            {product.careInstruction ? product.careInstruction
+            :
+              <p>no care Instructions available <br/>
+                Contact an employee for more details.
+              </p>
+            }
           </p>
-          {user && 
-            <button>Add To Cart</button>
-          }
+          
         </div>
       </div>
     </main>
