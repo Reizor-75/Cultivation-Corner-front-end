33e01d1924c2c990f92535ced5da87db581b1363
mlinares <m.linares93@gmail.com>
2024-10-18 22:44:42 -0400

    update landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,22 +4,25 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      {/* <h1>hello, {user ? user.name : 'friend'}</h1> */}
 
       <div className={styles.landing_container}>
-        <img src="" alt="slideshow of Plant Shop" />
+        <div className={styles.image_container}>
+          <img src="" alt="slideshow of Plant Shop" />
+        </div>
         <div className={styles.shop_details}>
           <div className={styles.map}>Map</div>
           <div className={styles.shop_info}>
             <div className="shop_hours">
               <h2>Hours</h2>
               <ul className={styles.hours}>
-                <li>Sun: 10:00 -6:00</li>
-                <li>Mon-Fri: 10:00-7:00</li>
+                <li className={styles.time_slot}>Sun: 10:00 -6:00</li>
+                <li className={styles.time_slot}>Mon-Fri: 10:00-7:00</li>
               </ul>
             </div>
             <div className="shop_address">
               <h2>Address</h2>
+              <p>Bowling Green, New York, NY 10004</p>
             </div>
           </div>
         </div>
