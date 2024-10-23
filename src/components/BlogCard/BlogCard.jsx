7ba803f5e57db398b60a0acc99b6633e94c84dd5
mlinares <m.linares93@gmail.com>
2024-10-23 13:58:43 -0400

    style blogcard

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -5,8 +5,11 @@ import styles from './BlogCard.module.css'
 const BlogCard = ({ blog }) => {
   return ( 
   <div className={styles.blog_container}>
-    <div className={styles.blog_title} >{blog.title}</div>
-    <p className={styles.blog_content}>{blog.content}</p>
+    <div className={styles.image_container}>
+      <img className={styles.blog_cover_photo} src="" alt="Cover Photo"/>
+    </div>
+    <div className={styles.blog_title}>{blog.title}</div>
+    <div className={styles.blog_content}>{blog.content}</div>
   </div> );
 }
 
