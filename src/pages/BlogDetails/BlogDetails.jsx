7897923200d4841393223529cc3fef23b2113b9f
mlinares <m.linares93@gmail.com>
2024-10-24 23:30:03 -0400

    fetch blog info to blogDetails

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -1,10 +1,29 @@
+// npm modules
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+//services
+import * as blogServices from '../../services/blogService' 
+
 // css
 import styles from './BlogDetails.module.css'
 
 const BlogDetails = () => {
+  const {blogId} = useParams()
+  const [blog, setBlog] = useState(null)
+
+  useEffect(()=>{
+
+    const fetchBlog = async () => {
+      const data = await blogServices.showBlog(blogId)
+      setBlog(data)
+    }
+    fetchBlog()
+  },[blogId])
+
   return (  
     <main className={styles.main_container}>
-      <h1>Blog Details</h1>
+      <h1>{blog?.title}</h1>
     </main>
   );
 }
