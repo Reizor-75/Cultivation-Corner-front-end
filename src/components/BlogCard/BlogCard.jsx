cebdac2fc0c32924f701b54b3f22685e4ac5c1fd
mlinares <m.linares93@gmail.com>
2024-10-24 23:24:31 -0400

    create navlink to blogDetails view

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,20 +1,25 @@
+// npm modules
+import { NavLink } from 'react-router-dom';
 
 // css
 import styles from './BlogCard.module.css'
 
 const BlogCard = ({ blog }) => {
   return ( 
-  <div className={styles.blog_container}>
-    <div className={styles.image_container}>
-      <img className={styles.blog_cover_photo} src="" alt="Cover Photo"/>
-    </div>
-    <div className={styles.blog_title}>{blog.title}</div>
-    <div className={styles.blog_content}>{blog.content}</div>
-    <div className={styles.read_more}>
-    <div className={styles.dash}>&#8212;</div>
-    <div className={styles.rm_text}>Read More</div>
-    </div>
-  </div> );
+    <NavLink to={`/blogs/${blog._id}`}>
+      <div className={styles.blog_container}>
+        <div className={styles.image_container}>
+          <img className={styles.blog_cover_photo} src="" alt="Cover Photo"/>
+        </div>
+        <div className={styles.blog_title}>{blog.title}</div>
+        <div className={styles.blog_content}>{blog.content}</div>
+        <div className={styles.read_more}>
+        <div className={styles.dash}>&#8212;</div>
+        <div className={styles.rm_text}>Read More</div>
+        </div>
+      </div>
+    </NavLink>
+  );
 }
 
 export default BlogCard;
