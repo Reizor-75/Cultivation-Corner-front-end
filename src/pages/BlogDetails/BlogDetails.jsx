c4acc0198c48cb76cb77f3be37ba65c0a17b5399
mlinares <m.linares93@gmail.com>
2024-10-24 23:19:10 -0400

    create blog Details view

---
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -0,0 +1,12 @@
+// css
+import styles from './BlogDetails.module.css'
+
+const BlogDetails = () => {
+  return (  
+    <main className={styles.main_container}>
+      <h1>Blog Details</h1>
+    </main>
+  );
+}
+
+export default BlogDetails;
