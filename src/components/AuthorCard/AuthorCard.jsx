70bbea570c4bb1b477a52597e0c9a43dd91b8ed3
mlinares <m.linares93@gmail.com>
2024-10-29 21:49:51 -0400

    style AuthorCard Component

--- src/components/AuthorCard/AuthorCard.jsx
+++ src/components/AuthorCard/AuthorCard.jsx
@@ -7,10 +7,10 @@ import defaultPic from '../../assets/DefaultMember.jpg'
 const AuthorCard = ({author}) => {
   return ( <div className={styles.author_container}>
     <div className={styles.image_container}>
-        <img className={styles.staff_mem_photo} src={author.photo ? author.photo : defaultPic} alt="Author picture placeHolder"/>
+        <img className={styles.author_photo} src={author.photo ? author.photo : defaultPic} alt="Author picture placeHolder"/>
         </div>
       <div className={styles.author_info}>
-        {author.name}
+        By {author.name}
       </div>
   </div> );
 }
