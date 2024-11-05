97407567b71195188338077b72c3b9c2cf241730
mlinares <m.linares93@gmail.com>
2024-11-05 08:53:40 -0500

    add authorCard component to commentCard

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,11 +1,15 @@
 // css
 import styles from './CommentCard.module.css'
 
+// components
+import AuthorCard from '../AuthorCard/AuthorCard';
+
 const CommentCard = ({comment}) => {
   return (  
     <div className={styles.comment_container}>
       <div className={styles.comment_title}>{comment.title}</div>
       <div className={styles.comment_content}>{comment.content}</div>
+      <AuthorCard author={comment.commenter}/>
     </div>
   );
 }
