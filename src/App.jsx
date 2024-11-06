a63ae7486b9a6bfa0c43eb35697f6ccecc01d038
mlinares <m.linares93@gmail.com>
2024-11-06 12:19:53 -0500

    create handleDeleteComment function

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,14 @@ function App() {
     navigate('/blogs')
   }
 
+  const handleDeleteComment = async (blogId, commentId) => {
+    const updatedBlog = blogs.find(blog => blog._id === blogId)    
+    await blogService.deleteComment(blogId, commentId)
+    setBlogs(blogs.map((blog) => updatedBlog._id === blog._id ?
+    {...blogs.find(blog => blog._id === blogId), comments: updatedBlog.comments.filter((comment) => comment._id !== commentId)}
+    :blog))
+    navigate(`/blogs`)
+  }
   return (
     <>
       <NavBar user={user} handleLogout={handleLogout} />
@@ -118,7 +126,9 @@ function App() {
           element={ <Blogs 
             user={user}
             blogs={blogs}
-            handleDeleteBlog={handleDeleteBlog}/> }
+            handleDeleteBlog={handleDeleteBlog}
+            handleDeleteComment={handleDeleteComment}
+          /> }
         />
         <Route
           path="/Blogs/:blogId"
