1fc9caffbe0487bda0882a5ef2281a8c3c91e094
mlinares <m.linares93@gmail.com>
2024-11-06 16:26:58 -0500

    style delete comment button

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -6,9 +6,11 @@ import AuthorCard from '../AuthorCard/AuthorCard';
 
 const CommentCard = ({comment, deleteComment}) => {
   return (  
-    <div className={styles.comment_container}>      
-      <AuthorCard author={comment.commenter}/>
-      <button onClick={()=> deleteComment(comment._id)}>Delete</button>
+    <div className={styles.comment_container}>  
+      <div className={styles.top_row}>    
+        <AuthorCard author={comment.commenter}/>
+        <button className={styles.delete_button}onClick={()=> deleteComment(comment._id)}>Delete</button>
+      </div>
       <div className={styles.comment_title}>{comment.title}</div>
       <div className={styles.comment_content}>{comment.content}</div>
       <hr/>
