ffab8cb2ebdb4e13d5b05c2fb5076e93e86469b2
mlinares <m.linares93@gmail.com>
2024-11-06 16:38:47 -0500

    create editBlog view template

---
+++ src/pages/EditBlog/EditBlog.jsx
@@ -0,0 +1,13 @@
+// css
+import styles from './EditBlog.module.css'
+
+const EditBlog = ({blog}) => {
+  return (
+    <main className={styles.edit_blog_container}>
+      <h1>blog.title</h1>
+    </main>
+  
+  );
+}
+
+export default EditBlog;
