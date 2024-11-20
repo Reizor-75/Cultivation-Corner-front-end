13a62782298669ba527880e01136855f3957d677
mlinares <m.linares93@gmail.com>
2024-11-19 22:08:44 -0500

    create footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+// css
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return ( 
+  <footer>
+    <div className={styles.socials}>
+
+    </div>
+    <div className={styles.mailing_list}>
+
+    </div>
+  </footer> 
+  );
+}
+ 
+export default Footer;
