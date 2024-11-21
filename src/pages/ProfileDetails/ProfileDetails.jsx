4c2c594f80ced93c9bdeeb3ec812528e2d714441
mlinares <m.linares93@gmail.com>
2024-11-21 11:14:35 -0500

    set up fetchProfile in profileDetails view

--- src/pages/ProfileDetails/ProfileDetails.jsx
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -1,10 +1,28 @@
+// npm modules
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+// services
+import * as profileServices from "../../services/profileService"
 
 // css
 import styles from './ProfileDetails.module.css'
 
 const ProfileDetails = () => {
+  const {profileId} = useParams()
+  const [profile, setProfile] = useState(null)
+
+  useEffect(() =>{
+    const fetchProfile= async () => {
+      const data = await profileServices.showProfile(profileId)
+      setProfile(data)
+    }
+    fetchProfile()
+  }, [profileId])
+
   return (
     <main className={styles.main_container}>
+      {profile?.name}
       <h1>test</h1>
     </main>
   );
