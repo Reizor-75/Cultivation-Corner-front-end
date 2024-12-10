b83ad7ac9180c75acad6cc3bad5bb806e32a7fd8
mlinares <m.linares93@gmail.com>
2024-12-09 22:50:47 -0500

    update Carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -4,12 +4,16 @@ import { useState, useEffect, useRef } from "react";
 // css
 import styles from "./Carousel.module.css"
 
+import plantShop from '../../assets/Store_Front.jpg'
+import def from '../../assets/DefaultMember.jpg'
+const images = [plantShop, def]
+
 const Carousel = () => {
   const [position, setPosition] = useState(0)
   const timeoutRef = useRef(null);
   const delay = 5000
 
-  const colors = ["#0088FE", "#00C49F", "#FFBB28"];
+  // const colors = ["#0088FE", "#00C49F", "#FFBB28"];
 
   function resetTimeout(){
     if (timeoutRef.current) clearTimeout(timeoutRef.current)
@@ -20,26 +24,26 @@ const Carousel = () => {
 
     timeoutRef.current = setTimeout(() =>
       setPosition((prevPos) =>
-        prevPos === colors.length - 1 ? 0 : prevPos + 1), delay )
+        prevPos === images.length - 1 ? 0 : prevPos + 1), delay )
       return () => { resetTimeout() }
-    }, [colors.length])
+    }, [])
 
-  // const images = []
+
   return (
     <div className={styles.carousel_container}>
       <div className={styles.slider}
         style={{ transform: `translate3d(${-position * 100}%, 0, 0)`}} 
       >
-      {colors.map((color, i) =>(
-        // <img className={styles.slide} key={i} src={image} alt="Slide"/>
-        <div 
-        className={styles.slide}
-        key={i} style={{backgroundColor: color}}>{color}</div>
+      {images.map((image, i) =>(
+        <img className={styles.slide} key={i} src={image} alt="Slide"/>
+        // <div 
+        // className={styles.slide}
+        // key={i} style={{backgroundColor: color}}>{color}</div>
       ))}
       </div>
 
       <div className={styles.dots_container}>
-        {colors.map((_, i) =>
+        {images.map((_, i) =>
         <div className={`${styles.dot}${position === i ? ` ${styles.active}` : ""}`} 
         key={i}
         onClick={() =>{setPosition(i)}}
